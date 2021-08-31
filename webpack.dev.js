2b175420d869d332230b85aeeab0a4776ed0fe68
mohamed <76411950+mmoha-199@users.noreply.github.com>
2021-09-01 00:53:28 +0100

    add webpack

---
+++ webpack.dev.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const loader = require('sass-loader')
+const WorkboxPlugin = require("workbox-webpack-plugin");
+
+
+module.exports = {
+    entry: './src/client/index.js',
+    output: {
+         libraryTarget: 'var',
+         library: 'Client'  ,
+        },
+    mode: 'development',
+    devtool: 'source-map',
+    stats: 'verbose',
+    target: 'node',
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test:/\.scss$/,
+                use:['style-loader', 'css-loader', 'sass-loader']
+            },
+           {
+                test: /\.scss$/,
+                use:[MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false,
+        }),
+        new MiniCssExtractPlugin({filename:"[name].css"}),
+        new WorkboxPlugin.GenerateSW(),
+    ]
+}
